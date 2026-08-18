09b783a0380cb3d891251777513b638b488a50c9
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-08-18 07:43:22 +0700

    feat(frontend): initialize next.js frontend application

---
+++ frontend/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="m-10">
+      <h1 className="text-2xl font-bold text-blue-500">Hello, World!</h1>
+    </div>
+  );
+}
